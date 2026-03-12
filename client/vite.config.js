fa079a89c80e754746e913a125bccb611abba058
Luizzz1114 <cortesialuis1114@gmail.com>
2026-03-12 19:00:12 -0400

    feat:Commit inicial
    Co-authored-by: zapata888 <zapata888gilberto@gmail.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
-import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
