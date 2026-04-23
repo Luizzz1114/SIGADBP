bdad5203b486365c26e431c64cb9e6c2a2895773
Luizzz1114 <cortesialuis1114@gmail.com>
2026-04-23 16:29:47 -0400

    fix: Arreglo del tamaño del boton para recuperar contraseña
    Co-authored-by: zapata888 <zapata888@users.noreply.github.com>

--- client/src/layout/LoginLayout.vue
+++ client/src/layout/LoginLayout.vue
@@ -2,10 +2,10 @@
 
 <template>
   <div class="flex justify-end w-dvw h-dvh bg-linear-to-b bg-slate-200 overflow-hidden dark:bg-slate-800">
-    <div class="flex flex-col items-center justify-center flex-1 md:p-4">
+    <div class="flex flex-col items-center justify-center flex-1 sm:p-4">
       <img src="../assets/images/analysis-bro.svg" class="max-w-md">
     </div>
-    <div class="flex flex-col items-center w-full h-full bg-white border-s border-slate-300 md:w-108 dark:bg-slate-850 dark:border-slate-700">
+    <div class="flex flex-col items-center w-full h-full bg-white border-s border-slate-300 sm:w-100 dark:bg-slate-850 dark:border-slate-700">
       <div class="flex items-center gap-3 px-8 py-6 w-full text-slate-700 dark:text-slate-200">
         <div class="flex items-center justify-center shrink-0 p-1 h-8 w-12 rounded-lg bg-blue-600">
           <img
